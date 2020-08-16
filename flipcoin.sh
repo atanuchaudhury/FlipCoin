43f3b18207ff9ef78b27ba70174d2be2ab8ceda2
atanuchaudhury <atanuchaudhury1994@gmail.com>
2020-08-16 18:45:54 +0530

    inputs from multiple flips recorded

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,11 +1,16 @@
-#! /bin/bash
-
+#!/bin/bash
+Head=0
+Tail=0
+for ((i=0; i<10; i++))
+do
 Flip=$((RANDOM%2))
-
 if [ $Flip -eq 1 ]
 then
-        echo Head
+         ((Head++))
 else
-        echo Tail
+         ((Tail++))
 fi
+done
+echo "total head count" $Head
+echo "total tail count" $Tail
 
