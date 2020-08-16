78d2c8b04042964c7853f2bf7f9f5859aad68a31
atanuchaudhury <atanuchaudhury1994@gmail.com>
2020-08-16 18:54:12 +0530

    a win or a tie

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,16 +1,38 @@
-#!/bin/bash
-Head=0
-Tail=0
-for ((i=0; i<10; i++))
+#! /bin/bash
+
+i=1
+count1=0;
+count2=0;
+while (( $i<=11 ))
 do
 Flip=$((RANDOM%2))
-if [ $Flip -eq 1 ]
-then
-         ((Head++))
-else
-         ((Tail++))
-fi
-done
-echo "total head count" $Head
-echo "total tail count" $Tail
 
+case $Flip in
+        1)
+        coin[1]=$((count1++))
+        if [[ $count1 -eq 21 ]]
+        then
+                break
+        fi     ;;
+        2)
+        coin[2]=$((count2++))
+        if [[ $count2 -eq 21 ]]
+        then
+                break
+        fi     ;;
+esac
+done
+
+echo ${!coin[@]}
+echo ${coin[@]}
+
+if [ $count1 -eq $count2 ]
+then
+        echo "Game tie"
+elif [ $count1 -gt $count2 ]
+then
+        echo "Head won the match by $count1 "
+else
+
+        echo "Tail won the match by $count2"
+fi
